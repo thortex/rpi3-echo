2dba9250ca03977b2eb4c7b89ee304315af431ec
Thor Watanabe <thormac@gmail.com>
2018-06-10 07:00:42 +0900

    Added news reader feature.

---
+++ mp3/anime/down2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh 
+wget -q -c https://soundeffect-lab.info/sound/anime/mp3/gust1.mp3
+wget -q -c https://soundeffect-lab.info/sound/machine/mp3/train-bell1.mp3
+wget -q -c https://soundeffect-lab.info/sound/machine/mp3/locomotive-whistle1.mp3
+wget -q -c https://soundeffect-lab.info/sound/machine/mp3/ship-big-whistle1.mp3
+wget -q -c https://soundeffect-lab.info/sound/machine/mp3/car-engine1.mp3
+wget -q -c https://soundeffect-lab.info/sound/animal/mp3/lion1.mp3
+wget -q -c https://soundeffect-lab.info/sound/animal/mp3/elephant1.mp3
+wget -q -c https://soundeffect-lab.info/sound/animal/mp3/dog2.mp3
+wget -q -c https://soundeffect-lab.info/sound/animal/mp3/cat-cry3.mp3
+wget -q -c http://taira-komori.jpn.org/sound/environment01/fireengine_starting.mp3
