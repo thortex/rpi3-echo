8d60c890efed3dc2fe20bf6dd71f268934e39755
Thor Watanabe <thormac@gmail.com>
2018-06-12 06:23:45 +0900

    canceled locating hts/julius files into /dev/shm.

--- 03_test_sound.sh
+++ 03_test_sound.sh
@@ -7,13 +7,15 @@ fi
 
 TMPFILE=/tmp/open_jtalk_tmp.wav
 
+#HTS=/dev/shm/naist-jdic/tohoku-f01-neutral.htsvoice
+#DIC_DIR=/dev/shm/naist-jdic
+HTS=/usr/share/hts-voice/htsvoice-tohoku-f01/tohoku-f01-neutral.htsvoice
+DIC_DIR=/var/lib/mecab/dic/open-jtalk/naist-jdic
+
 echo "$1" | \
   open_jtalk \
-  -m /dev/shm/naist-jdic/tohoku-f01-neutral.htsvoice \
-  -x /dev/shm/naist-jdic \
+  -m $HTS \
+  -x $DIC_DIR \
   -ow $TMPFILE && \
   sudo aplay -q $TMPFILE && \
   rm -f $TMPFILE
- 
-#  -x /var/lib/mecab/dic/open-jtalk/naist-jdic \
-#  -m /usr/share/hts-voice/htsvoice-tohoku-f01/tohoku-f01-neutral.htsvoice \
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-./10_cp-dic.sh
+#./10_cp-dic.sh
 ./06_test_julius.sh &
 sleep 5 && ./07_echo.py
  
