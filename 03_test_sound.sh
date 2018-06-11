1e0ba90fd71625630b4f08b2782f40cf27da13d0
Thor Watanabe <thormac@gmail.com>
2018-06-11 10:06:46 +0900

    copying julius/hts model files into tmpfs directory: /dev/shm.

--- 03_test_sound.sh
+++ 03_test_sound.sh
@@ -9,11 +9,11 @@ TMPFILE=/tmp/open_jtalk_tmp.wav
 
 echo "$1" | \
   open_jtalk \
-  -m /usr/share/hts-voice/htsvoice-tohoku-f01/tohoku-f01-neutral.htsvoice \
+  -m /dev/shm/naist-jdic/tohoku-f01-neutral.htsvoice \
   -x /dev/shm/naist-jdic \
   -ow $TMPFILE && \
   sudo aplay -q $TMPFILE && \
   rm -f $TMPFILE
  
 #  -x /var/lib/mecab/dic/open-jtalk/naist-jdic \
-
+#  -m /usr/share/hts-voice/htsvoice-tohoku-f01/tohoku-f01-neutral.htsvoice \
--- 10_cp-dic.sh
+++ 10_cp-dic.sh
@@ -1,3 +1,14 @@
 #!/bin/sh -x
-mkdir -p /dev/shm/naist-jdic
-cp /var/lib/mecab/dic/open-jtalk/naist-jdic/*.* /dev/shm/naist-jdic/
+
+dst=/dev/shm/naist-jdic/
+mkdir -p $dst
+
+cp /var/lib/mecab/dic/open-jtalk/naist-jdic/*.* $dst
+cp /usr/share/hts-voice/htsvoice-tohoku-f01/tohoku-f01-neutral.htsvoice $dst
+
+dst=/dev/shm/julius
+mkdir -p $dst
+cp julius-kits/dictation-kit-v4.4/model/lang_m/bccwj.60k.bingram $dst
+cp julius-kits/dictation-kit-v4.4/model/lang_m/bccwj.60k.htkdic $dst
+cp julius-kits/dictation-kit-v4.4/model/phone_m/jnas-tri-3k16-gid.binhmm $dst
+cp julius-kits/dictation-kit-v4.4/model/phone_m/logicalTri-3k16-gid.bin $dst
