54169560db60f1e685f2fed9cb4fb2b1614bfaef
Thor Watanabe <thormac@gmail.com>
2018-06-18 20:27:04 +0900

    Added Julius/OpenCV debian packages.

--- 00_prepare.sh
+++ 00_prepare.sh
@@ -1,6 +1,39 @@
 #!/bin/sh 
 
+sudo apt-get remove nuscratch \
+     scratch \
+     scratch2 \
+     squeak-plugins-scratch \
+     sonic-pi \
+     libreoffice \
+     libreoffice-avmedia-backend-gstreamer \
+     libreoffice-base \
+     libreoffice-base-core \
+     libreoffice-base-drivers \
+     libreoffice-calc \
+     libreoffice-common \
+     libreoffice-core \
+     libreoffice-draw \
+     libreoffice-gtk \
+     libreoffice-gtk2 \
+     libreoffice-impress \
+     libreoffice-java-common \
+     libreoffice-math \
+     libreoffice-report-builder-bin \
+     libreoffice-sdbc-hsqldb \
+     libreoffice-style-galaxy \
+     libreoffice-systray \
+     libreoffice-writer \
+     uno-libs3 \
+     ure \
+     claws-mail \
+     claws-mail-i18n \
+     minecraft-pi \
+     python-minecraftpi \
+     python-picraft \
+     python3-minecraftpi \
+     python3-picraft 
+
 sudo apt-get update
 sudo apt-get upgrade -y 
 sudo apt-get dist-upgrade -y
-
--- 21_build_tf_py3.x.sh
+++ 21_build_tf_py3.x.sh
@@ -1,41 +1,91 @@
-#!/bin/sh -x
+#!/bin/bash -x
 
-# https://github.com/samjabrahams/tensorflow-on-raspberry-pi/blob/master/GUIDE.md
+git clone --recurse-submodules https://github.com/tensorflow/tensorflow.git
 
-#sudo apt-get install pkg-config zip g++ zlib1g-dev unzip
-#sudo apt-get install python3-pip python3-numpy swig python3-dev
-#sudo pip3 install wheel
+sudo apt-get install libeigen2-dev libeigen3-dev
+sudo apt-get install \
+     libblas-dev \
+     liblapack-dev \
+     libatlas-base-dev \
+     python3-dev \
+     python3-setuptools \
+     python3-numpy \
+     python3-scipy \
+     python3-h5py \
+     gfortran
 
-## 2018/6/16 現在、gcc は 6.3.0 であるため、実行しない。
-##sudo apt-get install gcc-4.8 g++-4.8
-##sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 100
-##sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 100
+cd tensorflow
 
-#wget -c https://github.com/bazelbuild/bazel/releases/download/0.14.1/bazel-0.14.1-dist.zip
+grep -Rl 'lib64' | xargs sed -i 's/lib64/lib/g'
 
-#unzip -d bazel bazel-0.14.1-dist.zip
-cd bazel
+# ./configure
+# You have bazel 0.14.1- (@non-git) installed.
+# Please specify the location of python. [Default is /usr/bin/python]: /usr/bin/python3
+# 
+# 
+# Found possible Python library paths:
+# /usr/local/lib/python3.5/dist-packages
+# /usr/lib/python3/dist-packages
+# Please input the desired Python library path to use.  Default is [/usr/local/lib/python3.5/dist-packages]
+# 
+# Do you wish to build TensorFlow with jemalloc as malloc support? [Y/n]:
+# jemalloc as malloc support will be enabled for TensorFlow.
+# 
+# Do you wish to build TensorFlow with Google Cloud Platform support? [Y/n]: n
+# No Google Cloud Platform support will be enabled for TensorFlow.
+# 
+# Do you wish to build TensorFlow with Hadoop File System support? [Y/n]:
+# Hadoop File System support will be enabled for TensorFlow.
+# 
+# Do you wish to build TensorFlow with Amazon S3 File System support? [Y/n]: N
+# No Amazon S3 File System support will be enabled for TensorFlow.
+# 
+# Do you wish to build TensorFlow with Apache Kafka Platform support? [Y/n]: N
+# No Apache Kafka Platform support will be enabled for TensorFlow.
+# 
+# Do you wish to build TensorFlow with XLA JIT support? [y/N]: N
+# No XLA JIT support will be enabled for TensorFlow.
+# 
+# Do you wish to build TensorFlow with GDR support? [y/N]: N
+# No GDR support will be enabled for TensorFlow.
+# 
+# Do you wish to build TensorFlow with VERBS support? [y/N]: N
+# No VERBS support will be enabled for TensorFlow.
+# 
+# Do you wish to build TensorFlow with OpenCL SYCL support? [y/N]: N
+# No OpenCL SYCL support will be enabled for TensorFlow.
+# 
+# Do you wish to build TensorFlow with CUDA support? [y/N]: N
+# No CUDA support will be enabled for TensorFlow.
+# 
+# Do you wish to download a fresh release of clang? (Experimental) [y/N]: N
+# Clang will not be downloaded.
+# 
+# Do you wish to build TensorFlow with MPI support? [y/N]: N
+# No MPI support will be enabled for TensorFlow.
+# 
+# Please specify optimization flags to use during compilation when bazel option "--config=opt" is specified [Default is -march=native]:
+# 
+# 
+# Would you like to interactively configure ./WORKSPACE for Android builds? [y/N]:
+# Not configuring the WORKSPACE for Android builds.
+# 
+# Preconfigured Bazel build configs. You can use any of the below by adding "--config=<>" to your build command. See tools/bazel.rc for more details.
+# --config=mkl         # Build with MKL support.
+# --config=monolithic  # Config for mostly static monolithic build.
+# Configuration finished
 
-f=scripts/bootstrap/compile.sh
-x=`grep mx500M $f`
-if [ "x$x" = "x" ] ; then
-    sed -i 's/{JAVAC}"/{JAVAC}" -J-Xmx500M/;' $f
-fi
-
-# Bazelは32-bitはサポートしていないため、ビルドエラーが出る。
-# いくつかソースを修正する
-#diff -u third_party/ijar/mapped_file_unix.cc{.orig,}
-#--- third_party/ijar/mapped_file_unix.cc.orig2018-06-16 10:29:34.682306745 +0900
-#+++ third_party/ijar/mapped_file_unix.cc2018-06-16 10:29:50.272163155 +0900
-#-  size_t mmap_length = std::min(estimated_size + sysconf(_SC_PAGESIZE),
-#+  size_t mmap_length = std::min((size_t)(estimated_size + sysconf(_SC_PAGESIZE)),
-
-sudo ./compile.sh
-
-o=output/bazel
-if [ -e "$o" ] ; then
-    sudo cp $o /usr/local/bin/
-fi
-
-# https://github.com/samjabrahams/tensorflow-on-raspberry-pi/blob/master/GUIDE.md
+bazel build -c opt \
+      --config=monolithic \
+      --cpu=armeabi-v7a \
+      --copt=-mtune=cortex-a53 \
+      --copt=-mcpu=cortex-a53 \
+      --copt=-march=armv8-a+crc \
+      --copt=-mfpu=crypto-neon-fp-armv8 \
+      --copt=-funsafe-math-optimizations \
+      --copt=-ftree-vectorize \
+      --copt=-fomit-frame-pointer \
+      --verbose_failures \
+      --local_resources 1024,1.0,1.0 \
+      tensorflow/tools/pip_package:build_pip_package
 
